1a4dc40a8d3d34ed42add4cf56f5d9ba0c1088bc
Stanislav Vladislavovich <stanislav.vladis@yandex.ru>
2022-02-24 23:58:22 +0300

    Database connection implemented.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final String userName;
+    private final String password;
+    private final String connectionUrl;
+
+    public Util(String userName, String password, String connectionUrl) {
+        this.userName = userName;
+        this.password = password;
+        this.connectionUrl = connectionUrl;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(connectionUrl, userName, password);
+    }
+
 }
