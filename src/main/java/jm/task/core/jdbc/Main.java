31ff8c2bcce5afd555e42269aab81a0444c7352d
Stanislav Vladislavovich <stanislav.vladis@yandex.ru>
2022-02-25 19:46:03 +0300

    JDBC project realized

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,54 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
-
-
-
-        //Требования к классам приложения:
-
-        //Классы dao/service должны реализовывать соответствующие интерфейсы ОК
-        //Класс dao должен иметь конструктор пустой/по умолчанию ОК
-        //Все поля должны быть private
-        //service переиспользует методы dao
-        //Обработка всех исключений, связанных с работой с базой данных должна находиться в dao
-        //Класс Util должен содержать логику настройки соединения с базой данных
-
-
-         //Необходимые операции:
-
-         //Создание таблицы для User(ов) – не должно приводить к исключению, если такая таблица уже существует
-         //Удаление таблицы User(ов) – не должно приводить к исключению, если таблицы не существует
-         //Очистка содержания таблицы
-         //Добавление User в таблицу
-         //Удаление User из таблицы ( по id )
-         //Получение всех User(ов) из таблицы
-
-
-         //Алгоритм работы приложения:
-         //В методе main класса Main должны происходить следующие операции:
-
-         //Создание таблицы User(ов)
-         //Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
-         //Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
-         //Очистка таблицы User(ов)
-         //Удаление таблицы
-
-
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
 
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
-        //Получаем соединение с БД
-        Util util = new Util("root IDEA", "password", "jdbc:mysql://localhost:3306/kata_preproject_bd");
-        Connection connection = util.getConnection();
 
-
-
+        //1. Создание таблицы User(ов)
+        userService.createUsersTable();
+        //2. Добавление 4 User(ов) в таблицу с данными на свой выбор.
+        userService.saveUser("Коля", "Петров", (byte) 25);
+        userService.saveUser("Вася", "Иванов", (byte) 27);
+        userService.saveUser("Дима", "Маслеников", (byte) 18);
+        userService.saveUser("Саша", "Владимирович", (byte) 20);
+        //3. Получение всех User из базы и вывод в консоль
+        userService.getAllUsers().stream().forEach(System.out::println);
+        //4. Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+        //5. Удаление таблицы
+        userService.dropUsersTable();
     }
 
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,11 +1,26 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+//Данный класс реализует все функции необходимые для извлечения, обновления и удаления объектов User
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection connection;
+
     public UserDaoJDBCImpl() {
+
+        try {
+            connection = Util.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -13,30 +28,81 @@ public class UserDaoJDBCImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Users (Id MEDIUMINT NOT NULL AUTO_INCREMENT, Name CHAR(30) NOT NULL, LastName CHAR(30) NOT NULL, Age MEDIUMINT UNSIGNED NOT NULL, PRIMARY KEY (Id));");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void dropUsersTable() {
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS Users;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT Users(Name, LastName, Age) VALUES ('" + name + "', '" + lastName + "', " + age + ");");
+            System.out.println("User с именем – '" + name + "' добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void removeUserById(long id) {
 
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DELETE FROM Users WHERE Id = " + id + ";");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> list = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT Name, LastName, Age FROM Users;");
+
+            while (resultSet.next()) {
+                list.add(new User(resultSet.getString("Name"), resultSet.getString("LastName"), resultSet.getByte("Age")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+
     }
 
     @Override
     public void cleanUsersTable() {
 
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DELETE FROM Users;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
