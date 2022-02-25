1f43aa02134135a3c6b424bf0f3a63ca7c3c71f5
Stanislav Vladislavovich <stanislav.vladis@yandex.ru>
2022-02-25 13:09:02 +0300

    Updated database connection.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,24 +1,32 @@
 package jm.task.core.jdbc.util;
 
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private final String userName;
-    private final String password;
-    private final String connectionUrl;
 
-    public Util(String userName, String password, String connectionUrl) {
-        this.userName = userName;
-        this.password = password;
-        this.connectionUrl = connectionUrl;
-    }
+    public static Connection getConnection() throws SQLException {
 
-    public Connection getConnection() throws SQLException {
+        Properties properties = new Properties();
+        try (InputStream inputStream = Files.newInputStream(Paths.get("src", "main", "resources", "database.properties"))) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String connectionUrl = properties.getProperty("connectionUrl");
+        String userName = properties.getProperty("userName");
+        String password = properties.getProperty("password");
+
         return DriverManager.getConnection(connectionUrl, userName, password);
+
     }
 
 }
